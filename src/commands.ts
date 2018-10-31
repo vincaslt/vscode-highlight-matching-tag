ebbaaa6f2e8b8c6c0708272a7a5b62529b70b949
KamasamaK <KamasamaK@users.noreply.github.com>
2018-10-30 22:35:36 -0400

    Update scripts, tasks, launch, and formatting

--- src/commands.ts
+++ src/commands.ts
@@ -1,12 +1,12 @@
 import * as vscode from 'vscode'
-import { parseTags } from './tagParser';
-import { findMatchingTag } from './tagMatcher';
+import { findMatchingTag } from './tagMatcher'
+import { parseTags } from './tagParser'
 
 export async function jumpToMatchingTag() {
-  const editor = vscode.window.activeTextEditor;
+  const editor = vscode.window.activeTextEditor
 
   if (!editor) {
-    return;
+    return
   }
 
   const tagsList = parseTags(editor.document.getText())
@@ -16,18 +16,15 @@ export async function jumpToMatchingTag() {
   const match = findMatchingTag(tagsList, positionOffset)
 
   if (match && match.opening !== match.closing) {
-    let newPosition: vscode.Position;
     const openingTagStartPos = editor.document.positionAt(match.opening.start)
     const openingTagRange = new vscode.Range(
       openingTagStartPos,
       editor.document.positionAt(match.opening.end)
     )
 
-    if (openingTagRange.contains(position)) {
-      newPosition = editor.document.positionAt(match.closing.start).translate(0, 1)
-    } else {
-      newPosition = openingTagStartPos.translate(0, 1)
-    }
+    const newPosition = openingTagRange.contains(position)
+      ? editor.document.positionAt(match.closing.start).translate(0, 1)
+      : openingTagStartPos.translate(0, 1)
 
     editor.selection = new vscode.Selection(newPosition, newPosition)
     editor.revealRange(editor.selection)
