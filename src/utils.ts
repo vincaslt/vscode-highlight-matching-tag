c3230482cab1b1c5bab5a54c185c07e3a9d0f273
Vincas Stonys <vincaslt@gmail.com>
2018-11-10 10:55:42 +0200

    Clear active decorations cache

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function debounce<F extends (...params: any[]) => void>(fn: F, delay: number) {
-  let timeoutID: NodeJS.Timer
-  return function(this: any, ...args: any[]) {
-    clearTimeout(timeoutID)
-    timeoutID = setTimeout(() => fn.apply(this, args), delay)
-  } as F
-}
