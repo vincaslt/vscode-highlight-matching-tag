36f844aed52b99113f20c08ca2389531f6a0b880
Kashinath Patekar <42115482+helix026@users.noreply.github.com>
2019-11-09 17:47:21 +0530

    Fixes #97 Tags in commented code also highlighted
    The patch replaces comments in received copy of editor text with equal length string of spaces, thus avoids comments being considered as part of code.

--- src/extension.ts
+++ src/extension.ts
@@ -87,6 +87,16 @@ export function activate(context: vscode.ExtensionContext) {
 
       if (editorText !== editor.document.getText()) {
         editorText = editor.document.getText()
+        
+        // Preprocess text for comments (replace comments with string of spaces)
+        let match;
+        let regEx = /<!--((?!<!--)[\s\S])*-->/gm; //matches comments
+        while (match = regEx.exec(editorText)) {
+            let len = match[0].length
+            let repl = " ".repeat(len);
+            editorText = editorText.substring(0, match.index) + repl + editorText.substring(match.index + len); //replace comment with spaces
+        }
+        
         tagsList = parseTags(editorText, config.emptyElements)
       }
 
